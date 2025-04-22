95f854d76c99051d2c5b4311ed493057885db0c7
Dennis wambua <muasyathegreat4@gmail.com>
2025-04-22 15:58:38 +0300

    railway url added to list of allowed hosts

--- panacare/settings.py
+++ panacare/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-b5d_wilkzt5+d1cexz9dg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', 'panacaredjangobackend-production.up.railway.app').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,panacaredjangobackend-production.up.railway.app').split(',')
 
 
 # Application definition
