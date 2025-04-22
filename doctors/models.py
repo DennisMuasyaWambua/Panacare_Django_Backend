99c4b0bf6e14d2e0eab3c40b2bdd0d7b9249dd67
Dennis wambua <muasyathegreat4@gmail.com>
2025-04-22 16:19:29 +0300

    doctors education model added

--- doctors/models.py
+++ doctors/models.py
@@ -2,6 +2,12 @@ from django.db import models
 import uuid
 from django.conf import settings
 
+
+class Education(models.Model):
+    level_of_education = models.CharField(max_length=100)
+    field = models.CharField(max_length=100)
+    institution = models.CharField(max_length=100)
+
 class Doctor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='doctor')
@@ -9,7 +15,7 @@ class Doctor(models.Model):
     license_number = models.CharField(max_length=50)
     experience_years = models.PositiveIntegerField(default=0)
     bio = models.TextField(blank=True)
-    education = models.TextField(blank=True)
+    education = models.ForeignKey(Education, on_delete=models.CASCADE, related_name="doctor_education", null=False)
     is_verified = models.BooleanField(default=False)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -17,3 +23,4 @@ class Doctor(models.Model):
     
     def __str__(self):
         return f"Dr. {self.user.get_full_name()} - {self.specialty}"
+
