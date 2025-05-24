039459ad3d49897ec7111ff8aa0900cc40c05ab6
Dennis wambua <muasyathegreat4@gmail.com>
2025-05-24 16:22:38 +0300

    models imported to PatientListAPIView

--- users/views.py
+++ users/views.py
@@ -13,6 +13,7 @@ from django.shortcuts import get_object_or_404
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_str, force_bytes
 from django.contrib.auth.tokens import default_token_generator
+from django.db import models
 from rest_framework_simplejwt.tokens import RefreshToken
 from panacare.settings import SIMPLE_JWT
 from django.core.exceptions import ValidationError
